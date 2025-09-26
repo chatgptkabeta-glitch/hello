4ef3c8f8377ac948c0783ace41ea8d1ec9dd16f1
chatgptkabeta-glitch <chatgptkabeta@gmail.com>
2025-09-26 11:04:01 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,7 @@
+#Find Whether the Number is divisible by 2 and 3.
+
+a=int(input("Enter a Value ="))
+if (a%2==0 and a%3==0):
+    print("It is a divisible of 2 and 3.")
+else:
+    print("It is not a divisible of 2 and 3=")
